e60ae0aa1cd9c4833be32c0e5ef61a1b18337bc0
Trevor Stoddart <trevor.l.stoddart@gmail.com>
2017-03-25 19:21:24 +0000

    Created User Log-in / store current logged in user

--- cs5530/User.java
+++ cs5530/User.java
@@ -3,59 +3,115 @@ package cs5530;
 import java.sql.*;
 
 public class User {
+	
+	String name;
+	String username;
+	int type;
+	String password;
+	String phone_number;
+	String address;
+	
+	public User(){}
 
-		public User()
-		{}
+	public User(String name, String username, int type, String password, String phone_number, String address)
+	{
+		this.name = name;
+		this.username = username;
+		this.type = type;
+		this.password = password;
+		this.phone_number = phone_number;
+		this.address = address;
+	}
 
-                public void createUser(String name, String username, int type, String password, String phone_number, String address, Statement stmt) {
-
-                    String sql = "INSERT INTO user (name, username, type, password, phone_number, address) VALUES ('" + name + "', '" + username + "', " + type + ", '" + password + "', '" + phone_number + "', '" + address + "');";
-                    System.out.println("executing "+sql);
-                    try{
-                                stmt.executeUpdate(sql);
-                        }
-                        catch(Exception e)
-                        {
-                                System.out.println("cannot execute the query");
-                                e.printStackTrace();
-                        }
-                }
+    public void insertUser(Statement stmt) {
+    	String sql = "INSERT INTO user (name, username, type, password, phone_number, address) VALUES ('" + name + "', '" + username + "', " + type + ", '" + password + "', '" + phone_number + "', '" + address + "');";
+        System.out.println("executing "+sql);
+        try{
+        	stmt.executeUpdate(sql);
+            }
+        catch(Exception e)
+        {
+        	System.out.println("cannot execute the query");
+            e.printStackTrace();
+        }
+    }
 
 
 
 
 // todo: change to getUser.... 		
-		public String getCourse(String cname, String dname, Statement stmt)
-		{
-			String sql="select * from course where cname like '%"+cname+"%' and dname like '%"+dname+"%'";
-			String output="";
-			ResultSet rs=null;
-   		 	System.out.println("executing "+sql);
-   		 	try{
-	   		 	rs=stmt.executeQuery(sql);
-	   		 	while (rs.next())
-				 {
-					//System.out.print("cname:");
-				        output+=rs.getString("cname")+"   "+rs.getString("dname")+"\n"; 
-				 }
-			     
-			     rs.close();
-   		 	}
-   		 	catch(Exception e)
-   		 	{
-   		 		System.out.println("cannot execute the query");
-   		 	}
-   		 	finally
-   		 	{
-   		 		try{
-	   		 		if (rs!=null && !rs.isClosed())
-	   		 			rs.close();
-   		 		}
-   		 		catch(Exception e)
-   		 		{
-   		 			System.out.println("cannot close resultset");
-   		 		}
-   		 	}
-		    return output;
+	public void populateUser(String username, Statement stmt) throws Exception
+	{
+		String query;
+		String resultstr="";
+		ResultSet results; 
+		query="Select * from user where username ='"+username+"'";
+		try{
+			results = stmt.executeQuery(query);
+	       } catch(Exception e) {
+			System.err.println("Unable to execute query:"+query+"\n");
+	                System.err.println(e.getMessage());
+			throw(e);
+	       }
+		System.out.println("User.getUser Query = "+query+"\n");
+		while (results.next()){
+			setName(results.getString("name"));
+			System.out.println(getName());
+			setUsername(results.getString("username"));
+			setType(results.getInt("type"));
+			setPassword(results.getString("password"));
+			setPhoneNumber(results.getString("phone_number"));
+			setAddress(results.getString("address"));
 		}
+	}
+	
+	//Getters and Setters
+	public void setName(String name){
+		this.name = name;
+	}
+	
+	public String getName(){
+		return this.name;
+	}
+	
+	public void setUsername(String username){
+		this.username = username;
+	}
+	
+	public String getUsername(){
+		return this.username;
+	}
+
+	public void setType(int type){
+		this.type = type;
+	}
+
+	public int getType(){
+		return this.type;
+	}
+	
+	public void setPassword(String password){
+		this.password = password;
+	}
+	
+	public String getPassword(){
+		return this.password;
+	}
+	
+	public void setPhoneNumber(String phone_number)
+	{
+		this.phone_number = phone_number;
+	}
+	
+	public String getPhoneNumber(){
+		return this.phone_number;
+	}
+	
+	public void setAddress(String address){
+		this.address = address;
+	}
+	
+	public String getAddress(){
+		return this.address;
+	}
 }
